5d005e1293489fc8875ec26f0f516c2b4067dd03
Adriely Nara Freitas Barbosa (UFPE) <anfb@dmz.org.br>
2021-05-18 11:15:14 -0300

    Created SmartContext and created the relationship between Student and Course

--- SmartSchool.webAPI/Data/SmartContext.cs
+++ SmartSchool.webAPI/Data/SmartContext.cs
@@ -1,9 +1,19 @@
 using Microsoft.EntityFrameworkCore;
+using SmartSchool.webAPI.Models;
 
 namespace SmartSchool.webAPI.Data
 {
     public class SmartContext : DbContext
     {
-        
+        public SmartContext(DbContextOptions<SmartContext> options) : base(options) { }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StudentCourse> StudentsCourse { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<StudentCourse>().HasKey(AD => new { AD.StudentId, AD.CourseId }); //Many to many
+        }
     }
 }
