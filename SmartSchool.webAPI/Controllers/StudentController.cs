0270af53535cf1fda320468f1a6ad34c0de40d5b
Adriely Barbosa <adriely.nara@gmail.com>
2021-05-26 21:58:48 -0300

    using SmartContext in StudentController

--- SmartSchool.webAPI/Controllers/StudentController.cs
+++ SmartSchool.webAPI/Controllers/StudentController.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SmartSchool.webAPI.Data;
 using SmartSchool.webAPI.Models;
 
 namespace SmartSchool.webAPI.Controllers
@@ -10,76 +12,80 @@ namespace SmartSchool.webAPI.Controllers
     [Route("api/[controller]")] // api/controllerName
     public class StudentController : ControllerBase
     {
-        public List<Student> students = new List<Student>(){
-            new Student(){
-                Id = 1,
-                Name = "Carlos",
-                LastName = "Barbosa",
-                PhoneNumber = "(81) 9 8547-4145"
-            },
-            new Student(){
-                Id = 2,
-                Name = "Artur",
-                LastName = "Barbosa",
-                PhoneNumber = "(81) 9 8547-4145"
-            },
-            new Student(){
-                Id = 3,
-                Name = "João",
-                LastName = "Barbosa",
-                PhoneNumber = "(81) 9 8547-4145"
-            },
-            new Student(){
-                Id = 4,
-                Name = "Adriano",
-                LastName = "Barbosa",
-                PhoneNumber = "(81) 9 8547-4145"
-            }
-        };
-        public StudentController(){}
-
+        #region VARIABLES
+        private readonly SmartContext context;
+        #endregion
+        public StudentController(SmartContext context) 
+        {
+            this.context = context;
+        }
+    
+        #region PUBLIC METHODS
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok(students);
+            return Ok(this.context.Students);
         }
 
         // .../api/Student/byId?id=1
         [HttpGet("ById")]
         public IActionResult GetById(int id)
         {
-            Student student = students.FirstOrDefault(a => a.Id == id);
-            if(student == null) return BadRequest("Student was not found.");
+            Student student = this.context.Students.FirstOrDefault(a => a.Id == id);
+            if (student == null) return BadRequest("Student was not found.");
             return Ok(student);
         }
 
         // .../api/Student/byName?name=Fulano&lastname=barbosa
         [HttpGet("ByName")]
-        public IActionResult GetByName(string name, string lastName){
-            Student student = students.FirstOrDefault(a => a.Name.ToUpperInvariant().ToLowerInvariant().Contains(name.ToUpperInvariant().ToLowerInvariant()) 
-                                                        && a.LastName.ToUpperInvariant().ToLowerInvariant().Contains(lastName.ToUpperInvariant().ToLowerInvariant()));
-            if(student == null) return BadRequest("Student was not found.");
+        public IActionResult GetByName(string name, string lastName)
+        {
+            Student student = this.context.Students.FirstOrDefault(a => a.Name.Contains(name)
+                                                        && a.LastName.Contains(lastName));
+            if (student == null) return BadRequest("Student was not found.");
             return Ok(student);
         }
 
         [HttpPost]
-        public IActionResult PostStudent(Student student){
+        public IActionResult PostStudent(Student student)
+        {
+            this.context.Add(student);
+            this.context.SaveChanges();
             return Ok(student);
         }
 
         [HttpPut("{id}")]
-        public IActionResult PutSudent(int id, Student student){
+        public IActionResult PutSudent(int id, Student student)
+        {
+            Student sdt = this.context.Students.AsNoTracking().FirstOrDefault(s => s.Id == id);
+            if(sdt == null) return BadRequest("Invalid Path! Student does not exists.");
+            
+            this.context.Update(student);
+            this.context.SaveChanges();
             return Ok(student);
         }
 
         [HttpPatch("{id}")]
-        public IActionResult PatchSudent(int id, Student student){
+        public IActionResult PatchSudent(int id, Student student)
+        {
+            Student sdt = this.context.Students.AsNoTracking().FirstOrDefault(s => s.Id == id);
+            if(sdt == null) return BadRequest("Invalid Path! Student does not exists.");
+
+            this.context.Update(student);
+            this.context.SaveChanges();
             return Ok(student);
         }
 
         [HttpDelete("{id}")]
-        public IActionResult DeleteStudent(int id){
+        public IActionResult DeleteStudent(int id)
+        {
+            Student student = this.context.Students.FirstOrDefault(s => s.Id == id);
+            if (student == null) return BadRequest("Student does not exists.");
+
+            this.context.Remove(student);
+            this.context.SaveChanges();
             return Ok();
         }
+        #endregion  
     }
 }
